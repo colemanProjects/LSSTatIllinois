26b9cec8d6268d4f2594b1f2d15cd97ee60992af
root <root@lsst.cs.illinois.edu>
2013-06-28 16:05:45 -0500

    added to Readme. Got inital histogram showing.

--- index.php
+++ index.php
@@ -1,57 +1,33 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<? xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
  
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
- 
 <head>
 <!-- header for iipmooviewer-->
 
 <!-- header for lsst.cs.illinois.edu-->
 <meta http-equiv="Pragma" content="no-cache" />
 <meta http-equiv="Expires" content="-1" />
-
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
- 
 <meta name="description" content="" />
-	 
 	<meta name="keywords" content="" />
- 
 	<meta name="author" content="" />
-	 
 	<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
-	 
 	<title>SaoImage Ds9 Web</title>
 	<script src="jquery-ui-1.10.2.custom/js/jquery-1.9.1.js"></script> 
 	<script src="d3/d3.v3.min.js"></script> 
-	<script language= javascript>
-	</script> 
 	</head>
-	 
 	    <body>
-	 
 	        <div id="wrapper">
-	 
 	<?php include('includes/header.php'); ?>
-	 
 	<?php include('includes/nav.php'); ?>
-	 
 	<div id="content">
-
- 
 		<?php include('includes/menuBar.php'); ?>
 		<?php include('includes/dataDisplay.php'); ?>
  		<?php include('includes/centralFrame.php'); ?>
-	</div> <!-- end #content -->
-	 
+	</div> 
 	<?php include('includes/sidebar.php'); ?>
 	<?php include('includes/footer.php'); ?>
 	        </div> <!-- End #wrapper -->
-	 
 	    </body>
-<head>
-<meta http-equiv="Pragma" content="no-cache" />
-<meta http-equiv="Expires" content="-1" />
-</head>
-	 
-	</html>
-
+</html>
