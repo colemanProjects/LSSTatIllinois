3e897c0226df3fb329fb2645a3c23e944e37ff7b
root <root@lsst.cs.illinois.edu>
2013-07-10 01:09:05 -0500

    put the three sliders onto website

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,16 +1,15 @@
 <div id="sidebar">
- 
-<h3>Get Started</h3>
+	<h3>Get Started</h3>
+
+ 	<ul>
     <li>Upload Image</li>
     <li><iframe id = "uploadFrame" src="uploadPage.html" frameborder ="0" scrolling="no" width="100%" height="100px">
     </iframe></li>
-    <h3>Image Histogram</h3>
-    <li><iframe id = "histogramFrame" src="histogram.html" frameborder ="0" scrolling="no" width="100%" height="100px">
-    </iframe></li>
+    <li><h3>Image Histogram</h3></li>
+<!--	<li><h3 id="otherOptions">Other options are on their way!</h3></li> -->
+ 	</ul>
 
- 
-	<h3>Other options are on their way!</h3>
- 
+    <div id = "histogramContainer"><iframe id = "histogramFrame" src="hist.php"></iframe></div>
  
 </div> <!-- end #sidebar -->
 
